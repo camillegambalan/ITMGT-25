4837d53fb4bfe491ceea0b43dcd729f6f5c5a3b5
camillegambalan <maria.gambalan@obf.ateneo.edu>
2023-06-20 11:05:32 +0800

    Programming: Intermediate

--- YouTube Downloader/youtube-downloader.py
+++
@@ -1,8 +0,0 @@
-from pytube import YouTube
-
-yt = YouTube("https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUXbmV2ZXIgZ29ubmEgZ2l2ZSB5b3UgdXA%3D")
-
-yt.streams \
-    .filter(progressive=True, file_extension='mp4') \
-    .first() \
-    .download()
